bd61cfa1d1b863dc50eb6aff96cf934517154678
bp22083(mitobe kaito) <bp22083@sic.shibaura-it.ac.jp>
2025-02-18 00:42:03 +0900

    アプリ強制終了する問題の修正

--- app/src/main/java/supernova/MainActivity.kt
+++ app/src/main/java/supernova/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import supernova.R
 
 class MainActivity : AppCompatActivity() {
 
