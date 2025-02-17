234c4299ef4954dd867a37337969b833f8c2671e
bp22083(mitobe kaito) <bp22083@sic.shibaura-it.ac.jp>
2025-02-17 14:02:21 +0900

    1st commit

--- app/src/main/java/com/example/supernova/MainActivity.kt
+++ app/src/main/java/com/example/supernova/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SuperNovaTheme {
-        Greeting("Android")
+        Greeting("supernova")
     }
 }
